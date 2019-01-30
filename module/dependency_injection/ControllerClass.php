b554863a5bc2b1b0fee509f02e0296f99777ff1b
Anthony Fournier <anthony.fournier@mediacurrent.com>
2019-01-30 15:44:41 -0500

    Added services to controller DI example.

--- module/dependency_injection/ControllerClass.php
+++ module/dependency_injection/ControllerClass.php
@@ -4,6 +4,8 @@ namespace Drupal\my_module;
 
 use Drupal\Core\Logger\LoggerChannelFactory;
 use Drupal\Core\Session\AccountProxy;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 
 /**
  * The ControllerClass class.
@@ -25,11 +27,27 @@ class ControllerClass extends ControllerBase {
   protected $currentUser;
 
   /**
+   * The configuration factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
    * Constructs a new ControllerClass object.
    */
-  public function __construct(LoggerChannelFactory $logger_channel_factory, AccountProxy $account_proxy) {
+  public function __construct(LoggerChannelFactory $logger_channel_factory, AccountProxy $account_proxy, ConfigFactory $config_factory, EntityTypeManager $entity_type_manager) {
     $this->logger = $logger_channel_factory->get('my_logger_channel');
     $this->currentUser = $account_proxy;
+    $this->configFactory = $config_factory;
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
@@ -38,7 +56,9 @@ class ControllerClass extends ControllerBase {
   public static function create(ContainerInterface $container) {
     return new static(
     $container->get('logger.factory'),
-    $container->get('current_user')
+    $container->get('current_user'),
+    $container->get('config.factory'),
+    $container->get('entity_type.manager')
     );
   }
 
